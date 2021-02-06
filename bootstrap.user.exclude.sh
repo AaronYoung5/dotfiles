2a4f20d7e34799ead9110bb8cc22640ab06665c9
AaronYoung5 <aarony3131@gmail.com>
2021-02-06 16:51:07 -0600

    Added pseudo user stuff

---
+++ bootstrap.user.exclude.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+link() {
+  [ -z $USER_HOME ] && echo "USER_HOME is unset. Set it before continuing." && exit
+
+  read -p "USER_HOME is set to $USER_HOME. Is that okay? [yn] " -n 1 -r
+  echo
+
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+	  for file in $(ls -A | grep '\.user' | grep -vE '\.exclude*|\.git$|\.gitignore|\.gitmodules|.*.md|\.*.swp|\.last_update'); do
+	    # Silently ignore errors here because the files may already exist
+	    if [[ -f "$USER_HOME/$file" ]]; then
+		    echo "$USER_HOME/$file already exists. Please delete before continuing."
+		    continue
+	    fi
+	    ln -svf "$PWD/$file" "$USER_HOME" || true
+	  done
+  fi
+}
+
+link
+
+true
