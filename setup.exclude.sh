6a967cdf58525beb466906fa95b4f5c9dfc26feb
AaronYoung5 <aarony3131@gmail.com>
2021-02-07 12:56:48 -0600

    Testing setup script

--- setup.exclude.sh
+++ setup.exclude.sh
@@ -13,12 +13,13 @@ USER_HOME=$PWD
 ALIAS=$(basename $PWD)
 
 # Parse command line inputs
+# Get the positional arguments first
+# [ -z $1 ] && echo "Script type required." && help && exit
 while [[ $# -gt 0 ]]; do
 	key="$1"
 	case $key in
 		-u|--user_home)
 			[ -z $2 ] && echo "Please specify a user_home" && help && exit
-			[ ! -d $2 ] && echo "$2 not a directory. Creating it." && mkdir -p $2
 			USER_HOME=$2
 			shift
 			shift
@@ -32,9 +33,23 @@ while [[ $# -gt 0 ]]; do
 		*)
 			echo "Unknown option: $1"
 			echo "Ignoring..."
+			shift
 	esac
 done
 
+# Setup the pseudo user directory
+[ ! -d $USER_HOME ] && echo "$USER_HOME not a directory. Creating it." && mkdir -p $USER_HOME
+USER_HOME=$(readlink -f $USER_HOME)
+echo $USER_HOME
+cd $USER_HOME
+
 # Clone the repo
-# git clone git@github.com:AaronYoung5/dotfiles.git $PREFIX/.dotfiles
-# cd $PREFIX/.dotfiles
+git clone git@github.com:AaronYoung5/dotfiles .dotfiles
+cd .dotfiles
+
+# Run the bootstrap script
+USER_HOME=$USER_HOME ./bootstrap.user.exclude.sh
+
+# Add alias to the zsh_aliases.local file
+sed -i "/pseudousers/a alias $ALIAS=\"tu $USER_HOME $ALIAS\"" $HOME/.zsh_aliases.local
+
